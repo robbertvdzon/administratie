6f8f3d3b329789ea6363b7dfce2747cc9829efb6
Robbert <robbert@vdzon.com>
2016-05-08 20:01:57 +0200

    typescript errors fixed

--- applicatie/src/main/resources/web/lib/typings/browser.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="browser\ambient\angular-animate\angular-animate.d.ts" />
-/// <reference path="browser\ambient\angular\angular.d.ts" />
-/// <reference path="browser\ambient\require\require.d.ts" />
