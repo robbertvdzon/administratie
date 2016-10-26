1ee34bf07b71fe57fdce8b7d3eb613b8d2107a41
Robbert <robbert@vdzon.com>
2016-10-26 19:10:28 +0200

    opzet scala test

--- applicatie/src/main/scala/com/vdzon/administratie/checkandfix/actions/check/AfschriftCheck.scala
+++ applicatie/src/main/scala/com/vdzon/administratie/checkandfix/actions/check/AfschriftCheck.scala
@@ -1,15 +1,37 @@
 package com.vdzon.administratie.checkandfix.actions.check
 
 import java.util.Collection
+import java.util.stream.Collectors
+import collection.JavaConversions._
 
 import com.vdzon.administratie.checkandfix.CheckAndFixData
-import com.vdzon.administratie.checkandfix.model.CheckAndFixRegel
+import com.vdzon.administratie.checkandfix.model.{FixAction, CheckType, CheckAndFixRegel}
+import com.vdzon.administratie.model.boekingen.relaties.BoekingMetAfschrift
+import com.vdzon.administratie.model.{Afschrift, Factuur}
+import scala.compat.java8.StreamConverters._
 
 class AfschriftCheck {
-  @AdministratieCheckRule
-  def checkOfAfschriftNogBestaat(data: CheckAndFixData): Collection[_ <: CheckAndFixRegel] = {
-    println("CHECK VANUIT SCALA");
-    return null;
-  }
+//  @AdministratieCheckRule
+//  def checkOfAfschriftNogBestaat(data: CheckAndFixData): Collection[_ <: CheckAndFixRegel] = {
+//    return data.alleFacturen
+//          .stream.toScala[Stream].map(f=>checkFactuur(f,data)).map(f=>new CheckAndFixRegel(
+//      FixAction.NONE,
+//      "Factuur is niet volledig betaald of geboekt",
+//      "",
+//      "",
+//      CheckType.WARNING,
+//      null,
+//      null
+//    ));
+//  }
+////
+//  private def checkFactuur(factuur: Factuur, data: CheckAndFixData): Boolean = {
+//    val sumVanAfschriften: Double = data.boekingenCache.getBoekingenVanFactuur(factuur.getFactuurNummer).stream.mapToDouble(boeking => getAfschriftBedrag(data, boeking)).sum
+//    return sumVanAfschriften != factuur.getBedragIncBtw
+//  }
+//
+//  private def getAfschriftBedrag(data: CheckAndFixData, boeking: BoekingMetAfschrift): Double = {
+//    return data.alleAfschriften.stream.filter(afschrift => afschrift.getNummer().equals(boeking.getAfschriftNummer())).findFirst.orElse(Afschrift.builder.bedrag(0).build).getBedrag
+//  }
 
 }
--- applicatie/src/test/scala/com/vdzon/administratie/checkandfix/actions/check/AfschriftCheckTest.scala
+++ applicatie/src/test/scala/com/vdzon/administratie/checkandfix/actions/check/AfschriftCheckTest.scala
@@ -1,27 +1,35 @@
 package com.vdzon.administratie.checkandfix.actions.check
 
 
+import com.vdzon.administratie.checkandfix.CheckAndFixData
+import com.vdzon.administratie.model.boekingen.{BetaaldeFactuurBoeking, Boeking}
+import com.vdzon.administratie.model.{BoekingenCache, Factuur, Afschrift}
 import org.junit.runner.RunWith
 import org.scalatest._
 import org.scalatest.junit.JUnitRunner
 import collection.mutable.Stack
+import collection.JavaConversions._
+import scala.compat.java8.StreamConverters._
 
 @RunWith(classOf[JUnitRunner])
-class AfschriftCheckTest  extends FlatSpec with Matchers {
+class AfschriftCheckTest extends FlatSpec with Matchers {
 
-  "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new Stack[Int]
-    stack.push(1)
-    stack.push(2)
-    stack.pop() should be (2)
-    stack.pop() should be (1)
-  }
-
-  it should "throw NoSuchElementException if an empty stack is popped" in {
-    val emptyStack = new Stack[Int]
-    a [NoSuchElementException] should be thrownBy {
-      emptyStack.pop()
-    }
-  }
+//  "The AfschriftCheck" should "detect a problem" in {
+//    var checkAndFixData = new CheckAndFixData();
+//    checkAndFixData.alleAfschriften = List(new Afschrift(),new Afschrift())
+//    checkAndFixData.alleFacturen = List(new Factuur(),new Factuur())
+//    checkAndFixData.alleBoekingen = List(new BetaaldeFactuurBoeking(),new BetaaldeFactuurBoeking())
+//    checkAndFixData.boekingenCache = new BoekingenCache(checkAndFixData.alleBoekingen);
+//
+//    val regels = new AfschriftCheck().checkOfAfschriftNogBestaat(checkAndFixData);
+//    regels.size() should be (1)
+//  }
+//
+//  it should "throw NoSuchElementException if an empty stack is popped" in {
+//    val emptyStack = new Stack[Int]
+//    a [NoSuchElementException] should be thrownBy {
+//      emptyStack.pop()
+//    }
+//  }
 
 }
